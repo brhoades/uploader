2676b01478a8369debffb302bd29d46172c1198e
Billy Rhoades <bjrq48@mst.edu>
2014-02-14 15:50:56 -0600

    Added a umask option and included -p in scp.

--- upscreenshot.sh
+++ upscreenshot.sh
@@ -6,14 +6,16 @@ SITE="i.brod.es"
 USER="aaron"
 DIR="/var/www/images/$NAME"
 URL="http://$SITE/$NAME"
+UMASK=775
 PORT=22
 
-echo $URL | xsel --clipboard -i
-scrot -z -q 80 $@ $TEMPFILE
-scp -P $PORT "$TEMPFILE" $USER@$SITE:$DIR
-rm $TEMPFILE
+scrot -z -q 80 $@ "$TEMPFILE"
+chmod $UMASK "$TEMPFILE"
+scp -p -P $PORT "$TEMPFILE" $USER@$SITE:$DIR
 if [[ $? == 0 ]]; then
   notify-send 'Uploaded and pasted into clipboard.' $URL --icon=dialog-information
+  echo $URL | xsel --clipboard -i
 else
   notify-send 'Upload failed.' --icon=dialog-error
 fi
+rm "$TEMPFILE"
